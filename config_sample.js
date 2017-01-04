12e9998e4044c572469aa53f22ca64b1a1836118
Supriya B <supriya.bhat@weather.com>
2017-01-04 15:27:00 -0800

    sample config file

---
+++ config_sample.js
@@ -0,0 +1,24 @@
+// The attention word to wake up the robot.
+exports.attentionWord ='watson';
+
+// You can change the voice of the robot to your favorite voice.
+exports.voice = 'en-US_MichaelVoice';
+// Some of the available options are:
+// en-US_AllisonVoice
+// en-US_LisaVoice
+// en-US_MichaelVoice (the default)
+
+// Credentials for Watson Speech to Text service
+exports.STTUsername = 'xxx-xxx-xxx' ;
+exports.STTPassword = 'xxx-xxx-xxx' ;
+
+//Credentials for Watson Weather Insights service
+exports.WEATHERURL = 'xxx-xxx-xxx';
+exports.WEATHERUsername = 'xxx-xxx-xxx';
+exports.WEATHERPassword = 'xxx-xxx-xxx';
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = 'xxx-xxx-xxx' ;
+exports.TTSPassword = 'xxx-xxx-xxx' ; 
+
+exports.version = 'v1';
