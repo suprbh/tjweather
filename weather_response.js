686a2f52eedc9cee958a2b9b709968c280a559bf
Supriya B <supriya.bhat@weather.com>
2017-02-16 13:37:25 -0800

    handle empty data error

--- weather_response.js
+++ weather_response.js
@@ -8,42 +8,47 @@ exports.getWeatherData = function(intent, url, city, state, callback) {
 	request(url, function(error, response, body) {
 		if (!error && response.statusCode == 200){
 			var data = JSON.parse(body);
-			var latitude = data["location"]["latitude"][0];
-			var longitude = data["location"]["longitude"][0];
-			
-			console.log("Response: lat/lon: ", latitude, "/", longitude ); // Remove
-			
-			var conditionsUrl = config.WEATHERURL + "/api/weather/v1/geocode/" + latitude + "/" + longitude + "/observations.json?language=en-US&units=e";
-			// console.log("conditionsUrl ", conditionsUrl);
-			request(conditionsUrl, function(err, res, body){
-				// console.log("Conditions: ", err, " res", res, " body", body); // Remove
-				if(!err && res.statusCode == 200) {
-					var conditionsData = JSON.parse(body);
-					// console.log("Conditions Response: ", conditionsData); // Remove
-					var result = {
-						temp: conditionsData.observation.temp,
-						skyCondition: conditionsData.observation.wx_phrase,
-						windspeed: conditionsData.observation.wspd,
-						feels_like: conditionsData.observation.feels_like,
-						rain: conditionsData.observation.precip_hrly,
-						uv: conditionsData.observation.uv_desc,
-						city: city,
-						state: state,
-						intent: intent
-					};
+			if (data !== undefined) {
+				var latitude = data["location"]["latitude"][0];
+				var longitude = data["location"]["longitude"][0];
+				
+				console.log("Response: lat/lon: ", latitude, "/", longitude ); // Remove
+				
+				var conditionsUrl = config.WEATHERURL + "/api/weather/v1/geocode/" + latitude + "/" + longitude + "/observations.json?language=en-US&units=e";
+				// console.log("conditionsUrl ", conditionsUrl);
+				request(conditionsUrl, function(err, res, body){
+					// console.log("Conditions: ", err, " res", res, " body", body); // Remove
+					if(!err && res.statusCode == 200) {
+						var conditionsData = JSON.parse(body);
+						// console.log("Conditions Response: ", conditionsData); // Remove
+						var result = {
+							temp: conditionsData.observation.temp,
+							skyCondition: conditionsData.observation.wx_phrase,
+							windspeed: conditionsData.observation.wspd,
+							feels_like: conditionsData.observation.feels_like,
+							rain: conditionsData.observation.precip_hrly,
+							uv: conditionsData.observation.uv_desc,
+							city: city,
+							state: state,
+							intent: intent
+						};
 					
-					callback(false, result);
-						
-				}
-				else if(err) {
-					console.log("Conditions Error: ", err);
-					callback(true, err);
-				} 
-				else if(res.statusCode != 200) {
-					console.log("StatusCode error ", res.statusCode);
-					callback(true, res.statusCode);
-				}
-			});
+						callback(false, result);
+							
+					}
+					else if(err) {
+						console.log("Conditions Error: ", err);
+						callback(true, err);
+					} 
+					else if(res.statusCode != 200) {
+						console.log("StatusCode error ", res.statusCode);
+						callback(true, res.statusCode);
+					}
+				});
+			} else {
+				console.log("Weather API returned 200, but had empty response");
+				callback(true, "Sorry, there was an error trying to retrieve weather information for your location");
+			}
 		}
 		else if(error) {
 			console.log("Error in request: ", error);
